9ca6163e5523ab9a3218a05bac9bd2ab20c6b9f7
irbopia <travislockett1@gmail.com>
2021-09-25 15:59:03 -0400

    Chakra-UI installed - readme - logo

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { 
+	Flex,
+	Button,
+	Heading,
+	useColorMode,
+	useColorModeValue,
+} from "@chakra-ui/react";
+import { FcGoogle } from 'react-icons/fc'
+import { SunIcon } from '@chakra-ui/icons'
 
 const App = () => {
+
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue('gray.900');
   
   return (
-    <h1>Habit application</h1>
-  )
+		<ChakraProvider>
+			<Flex
+				background={formBackground}
+				height="100vh"
+				alignItems="center"
+				justifyContent="center"
+			>
+				<main>
+          <Heading
+            fontSize="4em"
+            color="blue.400"
+          >Habit Tribes</Heading>
+          <Button
+            rightIcon={<FcGoogle />}
+            backgroundColor="gray.600"
+            color="white"
+            fontWeight="medium"
+            mt={2}
+            _hover={{ bg: "gray.400" }}
+            _active={{
+              bg: "gray.800",
+              transform: "scale(0.95)",
+            }}
+            onClick={console.log('login')}
+          >Login with Google</Button>
+          <div></div>
+          <Button
+            rightIcon={<SunIcon />}
+						backgroundColor="gray.600"
+						color="white"
+						fontWeight="medium"
+						mt={2}
+						_hover={{ bg: "gray.400" }}
+						_active={{
+							bg: "gray.800",
+							transform: "scale(0.95)",
+						}}
+						onClick={toggleColorMode}
+					>
+						Color Mode
+					</Button>
+				</main>
+			</Flex>
+		</ChakraProvider>
+	);
 
 }
 
